864e1296118f88eaffed1cd53367be9fb5af9567
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-08-13 14:27:53 +0900

    Update Ex_15565.java

--- Ex_15565.java
+++ Ex_15565.java
@@ -11,10 +11,9 @@ public class Ex_15565
 		int N = sc.nextInt();
 		int K = sc.nextInt();
 		String dolls ;
-		for (int i = 0; i < N; i++)
-		{
-			dolls = sc.next();
-		}
+		
+		dolls = sc.next();
+		
 		sc.close();
 		
 		for (int j = 0; j < N; j++)
@@ -23,17 +22,20 @@ public class Ex_15565
 		}
 		
 		int[] lion = new int[1000000];
-		int k;
-		lion[k] = dolls.indexOf("1");
+		int m = 0;
+		
 		
 		if(dolls.length() >0)
 		{
-			int l, i =0;
+			int l=-110, i = 0;
+			int a = dolls.length();
 			do
 			{
 				l = dolls.indexOf("1", l + 1);
+				lion[m] = l;
+				m++;
 				System.out.println(i + "번 째 위치 : " +l);
-			}while(l+1 < dolls.length && l != -1);
+			}while(l+1 < dolls.length() && l != -1);
 		}
 	}
 }
