71f2af470d50f253e17651f563b3b3b701520cef
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-08-15 17:20:01 +0900

    Create Ex_2293.c

---
+++ Ex_2293.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+  int n, i, j, k, d[1001] = { 0 }, a[1001] = { 0 };
+  scanf("%d", &n);
+  scanf("%d", &k);
+  for (i = 1; i <= n; i++){
+    scanf("%d", &a[i]);
+  }
+  d[0] = 1;
+  for (i = 1; i <= n; i++){
+    for (j = a[i]; j <= k; j++){
+      // printf("d[%d] += d[%d - a[%d]] = ", j, j, i);
+      d[j] += d[j - a[i]];
+      // printf("%d\n", d[j]);
+    }
+  }
+  printf("%d", d[k]);
+}
