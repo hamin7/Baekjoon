bc7bb76383d71276691609333eafe584597a2389
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-08-22 13:44:42 +0900

    Update Ex_2558.java

--- Ex_2558.java
+++ Ex_2558.java
@@ -1,23 +1,11 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main{
+import java.util.*;
+public class Main {
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
         
-        String N = s.nextLine();
-        
-        int size = N.length();
-        char[] arr = new char[size];
-        
-        for(int i = 0; i < N.length(); i++){
-            arr[i] = N.charAt(i);
-        }
-        
-        Arrays.sort(arr);
-        
-        for(int i = arr.length - 1; i >= 0; i--){
-            System.out.print(arr[i]);
-        }
+         Scanner scan = new Scanner(System.in);
+         int A = scan.nextInt();
+         int B = scan.nextInt();     
+ 
+         System.out.println(A+B); 
     }
 }
