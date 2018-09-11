ab753ad196e64d80935aa046405683a2c276030a
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-09-11 10:21:23 +0900

    Add files via upload

---
+++ Ex9498.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int score;
+  cin >> score;
+
+  if(score>89)
+  {
+    cout << 'A' << endl;
+  }
+  else if(score>79)
+  {
+    cout << 'B' << endl;
+  }
+  else if(score>69)
+  {
+    cout << 'C' << endl;
+  }
+  else if(score>59)
+  {
+    cout << 'D' << endl;
+  }
+  else
+  {
+    cout << 'F' << endl;
+  }
+}
