79d0ac3872e94a33ba7574466b99562cf70fdc44
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-09-11 10:48:56 +0900

    Add files via upload

---
+++ Ex_2439.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int n,i;
+	cin >> n;
+
+	for(i = 1; i <= n; ++i){
+		for(int j = 1; j <= n-i; ++j)
+		{
+			cout << " ";
+		}
+		for(int k = 1; k <= i; ++k)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
