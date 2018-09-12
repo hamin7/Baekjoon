e21319d89d583dffc39f8317b275ac2317de7e75
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-09-12 14:58:56 +0900

    Update Ex_1463.cpp

--- Ex_1463.cpp
+++ Ex_1463.cpp
@@ -13,27 +13,23 @@ class makeOne
 	void setNum()
 	{
 		cin >> n;
-		memset(arr, 0, sizeof(int) * (n+1));     // 1부터 n까지 이므로 n+1.
+		memset(arr, 0, sizeof(int) * (n+1));     
 	}
-	//bottom up
-	// 1을 뺴는 계산부터 시작해서 작은 계산을 통해 미리 arr[i]에 (최대)값을 메모이제이션을 합니다.
-	// 2로 나누어 떨어지는지 3으로 나누어 떨어지는 경우에 앞에 계산한 arr[i/2]와 arr[i/3]과 현재의 arr[i]와 비교를 하여
-	// 그 중 최소 값을 arr[i]에 다시 저장(메모이제이션) 합니다.
-
+	
 	void solution()
 	{
-		arr[1] = 0;     // 1일때는 횟수 0
+		arr[1] = 0;     
 
 		for(int i = 2; i <= n; i++)
 		{
-			arr[i] = arr[i-1] + 1;    // 규칙 3번 : 1을 뺸다.
+			arr[i] = arr[i-1] + 1;   
 			if(i%2 == 0)
 			{
-				arr[i] = min(arr[i], arr[i/2] + 1);     // 구칙 2번 : : 2로 떨어지는 경우
+				arr[i] = min(arr[i], arr[i/2] + 1);     
 			}
 			if(i%3 == 0)
 			{
-				arr[i] = min(arr[i], arr[i/3] + 1);     // 규칙 1번 : 3으로 떨어지는 경우.
+				arr[i] = min(arr[i], arr[i/3] + 1);     
 			}
 		}
 	}
